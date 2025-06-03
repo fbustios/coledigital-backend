e04c90e3cce72665402310dc19e27793275967f4
Franco Bustios <francobustios07@gmail.com>
2025-06-03 14:17:11 -0600

    3/6/2025

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,8 +1,15 @@
 const bd = require('../config/db');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 
 exports.addStudent = (req,res) => {
     const {nombre, cedula, correo, telefono, fecha} = req.body;
     const contrasena = bcrypt.hashSync(nombre+cedula, 10);
+    const queryEstudiante = 'INSERT INTO Estudiante(cedula,nombre,correo,telefono,fechaNacimiento) values(?,?,?,?,?)';
+    const queryUser = 'INSERT INTO Usuarios(username,rol,password_hash,nombre) values (?,?,?,?)';
+    bd.query(query,[nombre, cedula, correo,telefono,fecha],(err,result)=>{
+        if(err){
+            return res.status(500).send({err})
+        }
+    });
     res.json();
 }
