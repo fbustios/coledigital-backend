5dee2f89c136dda6f55ac593e8349334e841fd64
fbustios <francobustios07@gmail.com>
2025-06-03 21:25:08 -0600

    adminFunctions

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,11 +6,23 @@ exports.addStudent = (req,res) => {
     const contrasena = bcrypt.hashSync(nombre+cedula, 10);
     const queryEstudiante = 'INSERT INTO Estudiante(cedula,nombre,correo,telefono,fechaNacimiento) values(?,?,?,?,?)';
     const queryUser = 'INSERT INTO Usuarios(username,rol,password_hash,nombre) values (?,?,?,?)';
-    bd.query(queryEstudiante,[nombre, cedula, correo,telefono,fecha],(err,result)=>{
-        if(err){
-            return res.status(500).send({err})
+    bd.query(queryEstudiante, [cedula, nombre, correo, telefono, fecha], (err, result) => {
+        if (err) {
+            console.error('error', err);
+            return res.status(500).json({message: 'Error'});
         }
+        bd.query(queryUser,[cedula,'Estudiante',contrasena,nombre],(err2,result) => {
+            if(err2){
+                console.error('error en users',err2);
+                return res.status(500).json({message : 'Error'})
+            }
+            res.status(201).json({message: 'Registro exitoso'});
+            console.log('Registro exitoso');
+        })
 
     });
-    res.json();
+
+
+
+
 }
