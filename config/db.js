94f9a97db75c76a7ce047f0fa0559d3cdbce03a7
fbustios <francobustios07@gmail.com>
2025-06-03 23:15:38 -0600

    adminFunctions

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 const database = process.env["DB_NAME"];
 
 const connection = mysql2.createConnection({
-    host:'192.168.100.16',
+    host:'6.tcp.ngrok.io',
+    port:'13758',
     user:'username',
     password:'remoto29072108',
     database:'coledigital',
